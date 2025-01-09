6ef2038fc2b752bf5fa73a26a5e3546e96aeed68
klpark79 <klpark79@naver.com>
2025-01-09 10:12:29 +0900

    111

---
+++ src/main/java/com/pkr/project/PkrProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pkr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkrProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkrProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkr/project/PkrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkr.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
